0a2061dc0083831355fe7c8c3884d1ec2d546441
jessica <57881206+fat-beaver@users.noreply.github.com>
2022-04-02 16:52:28 +1300

    Changed movement to a click to measure then click again to confirm system

--- Battle.gd
+++ Battle.gd
@@ -46,10 +46,7 @@ func _process(delta):
 	camera.move_local_x(movement_vector.x * camera_movement_mult * delta * zoom_level)
 	camera.move_local_y(movement_vector.y * camera_movement_mult * delta * zoom_level)
 	
-	if Input.is_action_pressed("left_click"):
-		_find_path_to_mouse()
-
-	if Input.is_action_just_released("left_click"):
+	if Input.is_action_just_pressed("left_click"):
 		_move_critter_to_mouse(ferret)
 
 	if Input.is_action_just_released("zoom_out") and zoom_level < max_zoom:
@@ -60,22 +57,14 @@ func _process(delta):
 		zoom_level -= 0.25
 		camera.zoom = Vector2(zoom_level, zoom_level)
 
-func _find_path_to_mouse():
-	var mouse_hex = grid.get_hex_at_coords(grid.hex_coords_of_point(get_global_mouse_position()))
-	if last_mouse_hex != mouse_hex and mouse_hex != null:
-		var path_start = ferret.get_location()
-		var path_end = mouse_hex
-		path.set_path(grid.find_path_between(path_start, path_end, default_unit_direction))
-		last_mouse_hex = mouse_hex
-
 func _move_critter_to_mouse(critter: Critter):
 	var mouse_hex = grid.get_hex_at_coords(grid.hex_coords_of_point(get_global_mouse_position()))
 	if mouse_hex != null:
-		var path_start = ferret.get_location()
+		var path_start = critter.get_location()
 		var path_end = mouse_hex
-		path.set_path(grid.find_path_between(path_start, path_end, default_unit_direction))
-		if path.get_path_length() != 0:
+		if mouse_hex != last_mouse_hex:
+			path.set_path(grid.find_path_between(path_start, path_end, default_unit_direction))
+		else:
 			critter.move(path_end)
-			critter.set_direction(path.get_end()[1])
 			path.clear_path()
 		last_mouse_hex = mouse_hex
